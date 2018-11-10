48f4f1f27c887ec97dd28fd31184dfa265a089e4
Dmitry <dmitriy.orenburg@gmail.com>
2018-11-10 06:28:31 -0800

    Added a privimitive link.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div onClick={this.props.onClick}>We're going to start {this.props.title}!</div>
+        <div>
+          <div onClick={this.props.onClick}>We're going to start {this.props.title}!</div>
+          <p><Link to="/about">All about gardening</Link></p>
+        </div>
     );
   }
 }
