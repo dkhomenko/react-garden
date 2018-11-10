10972a8a986745dbd03256c7e87d23a9d7b8eae6
Dmitry <dmitriy.orenburg@gmail.com>
2018-11-10 05:56:17 -0800

    Create the first event handler.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 let model = { title: "gardening" };
-ReactDOM.render(<App title={model.title} />, document.getElementById('root'));
-
+function render() {
+    ReactDOM.render(<App title={model.title} onClick={() => { model.title = model.title + '!'; render(); } } />, document.getElementById('root'));
+}
+render();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
