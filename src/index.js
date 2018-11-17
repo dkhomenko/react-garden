9fcbfe2a9576236e9031665dae250e5281e52860
Dmitry <dmitriy.orenburg@gmail.com>
2018-11-17 04:49:54 -0800

    Moved components to a separate folder.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
-import Home from './Home';
+import Home from './components/Home';
 import * as serviceWorker from './serviceWorker';
 
 let model = { title: "gardening" };
