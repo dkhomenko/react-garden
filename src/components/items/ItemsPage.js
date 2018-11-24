71943917a77913f44c38bb3b5755eb9f6ecc56ac
Dmitry <dmitriy.orenburg@gmail.com>
2018-11-24 05:50:37 -0800

    Refactored the Add logic into the parent component.

--- src/components/items/ItemsPage.js
+++ src/components/items/ItemsPage.js
@@ -6,9 +6,20 @@ import ItemApi from '../../api/mockItemApi';
 class ItemsPage extends Component {
     constructor(props) {
         super(props);
-        this.state = { items: [] };
+        
+        var newItem = {
+            name: '',
+            plantFamily: ''
+        };
+        
+        this.state = { 
+            items: [], 
+            newItem: newItem 
+        };
 
         this.addItem = this.addItem.bind(this);
+        this.onAddFieldChange = this.onAddFieldChange.bind(this);
+        this.clearNewItem = this.clearNewItem.bind(this);
     }
 
     componentDidMount() {
@@ -17,10 +28,29 @@ class ItemsPage extends Component {
         });
     }
 
-    addItem(item) {
-        ItemApi.saveItem(item).then((newItem) => {
-            var newItems = this.state.items.concat(newItem);
-            this.setState({ items: newItems });
+    onAddFieldChange(event) {
+        this.setState({
+            newItem: Object.assign(this.state.newItem, {[event.target.name]: event.target.value})
+        });
+    }
+
+    addItem(e) {
+        e.preventDefault();
+
+        ItemApi.saveItem(this.state.newItem).then((savedItem) => {
+            var items = this.state.items.concat(savedItem);
+            this.setState({ items: items });
+
+            this.clearNewItem();
+        });
+    }
+
+    clearNewItem() {
+        this.setState({
+            newItem: Object.assign(this.state.newItem, {
+                name: '',
+                plantFamily: ''
+            })
         });
     }
     
@@ -28,7 +58,7 @@ class ItemsPage extends Component {
         return (
             <div>
                 <ItemList items={this.state.items} />
-                <AddItem addItem={this.addItem} />
+                <AddItem item={this.state.newItem} addItem={this.addItem} onFieldChange={this.onAddFieldChange} />
             </div>
         );
     }
